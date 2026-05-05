2deee88da743cc6f18686d6979bbe4fdf3e2556f
strewili <shouqaaa123@gmail.com>
2026-05-05 23:23:49 +0300

    Update 105-radix_sort.c

--- 105-radix_sort.c
+++ 105-radix_sort.c
@@ -2,75 +2,59 @@
 #include <stdlib.h>
 
 /**
- * get_max - gets maximum value in array
+ * get_max - gets the maximum value in the array
  * @array: array
- * @size: size
+ * @size: size of array
  *
  * Return: max value
  */
-int get_max(int *array, size_t size)
+static int get_max(int *array, size_t size)
 {
 	size_t i;
 	int max;
 
 	max = array[0];
-	i = 1;
-
-	while (i < size)
+	for (i = 1; i < size; i++)
 	{
 		if (array[i] > max)
 			max = array[i];
-		i++;
 	}
 	return (max);
 }
 
 /**
- * counting_sort - stable counting sort for radix
+ * counting_sort_radix - counting sort used by radix sort
  * @array: array
  * @size: size
- * @exp: exponent (digit place)
+ * @exp: current digit
  * @output: temp array
  */
-void counting_sort(int *array, size_t size, int exp, int *output)
+static void counting_sort_radix(int *array, size_t size, int exp, int *output)
 {
 	int count[10] = {0};
 	size_t i;
 
-	i = 0;
-	while (i < size)
-	{
+	for (i = 0; i < size; i++)
 		count[(array[i] / exp) % 10]++;
-		i++;
-	}
 
-	i = 1;
-	while (i < 10)
-	{
+	for (i = 1; i < 10; i++)
 		count[i] += count[i - 1];
-		i++;
-	}
 
-	i = size;
-	while (i > 0)
+	for (i = size; i > 0; i--)
 	{
-		output[count[(array[i - 1] / exp) % 10] - 1] = array[i - 1];
+		output[count[(array[i - 1] / exp) % 10] - 1] =
+			array[i - 1];
 		count[(array[i - 1] / exp) % 10]--;
-		i--;
 	}
 
-	i = 0;
-	while (i < size)
-	{
+	for (i = 0; i < size; i++)
 		array[i] = output[i];
-		i++;
-	}
 }
 
 /**
  * radix_sort - sorts array using LSD radix sort
  * @array: array
- * @size: size
+ * @size: size of array
  */
 void radix_sort(int *array, size_t size)
 {
@@ -86,12 +70,10 @@ void radix_sort(int *array, size_t size)
 
 	max = get_max(array, size);
 
-	exp = 1;
-	while (max / exp > 0)
+	for (exp = 1; max / exp > 0; exp *= 10)
 	{
-		counting_sort(array, size, exp, output);
+		counting_sort_radix(array, size, exp, output);
 		print_array(array, size);
-		exp *= 10;
 	}
 
 	free(output);
