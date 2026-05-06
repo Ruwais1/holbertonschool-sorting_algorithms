8dc9343d5c06a042deb289c3036819ac1ff7a022
strewili <shouqaaa123@gmail.com>
2026-05-06 22:25:43 +0300

    Update sort.h

--- sort.h
+++ sort.h
@@ -31,6 +31,7 @@ void cocktail_sort_list(listint_t **list);
 void counting_sort(int *array, size_t size);
 void merge_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
 
 /* heap sort */
 void heap_sort(int *array, size_t size);
