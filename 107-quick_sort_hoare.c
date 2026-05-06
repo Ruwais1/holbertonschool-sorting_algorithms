634b55b2e85c6a0bf825316b525479cac02867e5
strewili <shouqaaa123@gmail.com>
2026-05-06 22:27:25 +0300

    Update 107-quick_sort_hoare.c

--- 107-quick_sort_hoare.c
+++ 107-quick_sort_hoare.c
@@ -2,10 +2,6 @@
 
 /**
  * swap_ints - swaps two integers in an array
- * @a: first integer
- * @b: second integer
- * @array: array
- * @size: size of array
  */
 void swap_ints(int *a, int *b, int *array, size_t size)
 {
@@ -21,31 +17,25 @@ void swap_ints(int *a, int *b, int *array, size_t size)
 }
 
 /**
- * hoare_partition - Hoare partition scheme
- * @array: array
- * @low: start index
- * @high: end index
- * @size: size of array
- *
- * Return: partition index
+ * hoare_partition - Hoare partition
  */
 int hoare_partition(int *array, int low, int high, size_t size)
 {
-	int pivot, i, j;
-
-	pivot = array[high];
-	i = low - 1;
-	j = high + 1;
+	int pivot = array[high];
+	int i = low - 1;
+	int j = high + 1;
 
 	while (1)
 	{
+		/* move i right */
 		do {
 			i++;
-		} while (array[i] < pivot);
+		} while (i <= high && array[i] < pivot);
 
+		/* move j left */
 		do {
 			j--;
-		} while (array[j] > pivot);
+		} while (j >= low && array[j] > pivot);
 
 		if (i >= j)
 			return (j);
@@ -55,33 +45,27 @@ int hoare_partition(int *array, int low, int high, size_t size)
 }
 
 /**
- * quick_sort_hoare_rec - recursive quick sort
- * @array: array
- * @low: start index
- * @high: end index
- * @size: size
+ * quick_sort_rec - recursive
  */
-void quick_sort_hoare_rec(int *array, int low, int high, size_t size)
+void quick_sort_rec(int *array, int low, int high, size_t size)
 {
 	int p;
 
 	if (low < high)
 	{
 		p = hoare_partition(array, low, high, size);
-		quick_sort_hoare_rec(array, low, p, size);
-		quick_sort_hoare_rec(array, p + 1, high, size);
+		quick_sort_rec(array, low, p, size);
+		quick_sort_rec(array, p + 1, high, size);
 	}
 }
 
 /**
- * quick_sort_hoare - sorts using Hoare scheme
- * @array: array
- * @size: size
+ * quick_sort_hoare - main
  */
 void quick_sort_hoare(int *array, size_t size)
 {
 	if (!array || size < 2)
 		return;
 
-	quick_sort_hoare_rec(array, 0, size - 1, size);
+	quick_sort_rec(array, 0, size - 1, size);
 }
