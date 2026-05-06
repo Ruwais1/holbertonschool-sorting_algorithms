c235f7a0ffcd9e26e23b912629554ea632a69a7f
strewili <shouqaaa123@gmail.com>
2026-05-06 22:30:44 +0300

    Update 107-quick_sort_hoare.c

--- 107-quick_sort_hoare.c
+++ 107-quick_sort_hoare.c
@@ -17,7 +17,7 @@ void swap_ints(int *a, int *b, int *array, size_t size)
 }
 
 /**
- * hoare_partition - partition using Hoare scheme
+ * hoare_partition - Hoare partition scheme
  */
 int hoare_partition(int *array, int low, int high, size_t size)
 {
@@ -27,10 +27,12 @@ int hoare_partition(int *array, int low, int high, size_t size)
 
 	while (1)
 	{
+		/* move i right */
 		do {
 			i++;
 		} while (array[i] < pivot);
 
+		/* move j left */
 		do {
 			j--;
 		} while (array[j] > pivot);
@@ -38,13 +40,12 @@ int hoare_partition(int *array, int low, int high, size_t size)
 		if (i >= j)
 			return (j);
 
-		if (array[i] != array[j])
-			swap_ints(&array[i], &array[j], array, size);
+		swap_ints(&array[i], &array[j], array, size);
 	}
 }
 
 /**
- * quick_sort_rec - recursive function
+ * quick_sort_rec - recursive helper
  */
 void quick_sort_rec(int *array, int low, int high, size_t size)
 {
@@ -53,6 +54,7 @@ void quick_sort_rec(int *array, int low, int high, size_t size)
 	if (low < high)
 	{
 		p = hoare_partition(array, low, high, size);
+
 		quick_sort_rec(array, low, p, size);
 		quick_sort_rec(array, p + 1, high, size);
 	}
